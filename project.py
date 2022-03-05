e1dec1a1000ae7e1453532b0b39776abe022c125
Bethany Vella <bethany.vella10@gmail.com>
2022-03-05 19:33:17 +0100

    Scraping top 100 sites for server name

--- project.py
+++ project.py
@@ -2,7 +2,10 @@ from urllib import response
 from bs4 import BeautifulSoup
 import requests
 import urllib.request
+from csv import reader
+import pandas as pd
 
+"""
 #try 1 - BV - not really sure about this one..need to do more research
 url =  'http://www.bethanyvella.com/'
 response = requests.get(url)
@@ -12,8 +15,41 @@ soup =  BeautifulSoup(response.text, 'html.parser')
 print(soup.title)
 print(soup)
 
+"""
+
 #try2 - RA
-url =  'http://www.bethanyvella.com/'
+"""
+filename ='tranco_QYW4.csv'
+df = pd.read_csv(filename)
+print(df)
+
+for count, url in df.iterrows():
+    response = urllib.request.urlopen(url)
+    print(url)
+    if count == 99:
+        break 
+
+
+"""
+read_obj = open('tranco_QYW4.csv', 'r') 
+csv_reader =  reader(read_obj)
+#csv_reader = ['google.com', 'youtube.com', 'facebook.com']
+for count, url in enumerate(csv_reader):
+    new_url = url[0] 
+    try:
+        response = urllib.request.urlopen('https://'+new_url)
+        print(new_url , response.info().get('Server'))
+    except urllib.error.HTTPError as e:
+        print(new_url , e.headers.get('Server'))
+    except urllib.error.URLError as e:
+        print(new_url, 'FAILED:', e.reason)
+    
+    if count == 99:
+        break
+        
+
+#url =  'http://www.bethanyvella.com/'
+"""
 response = urllib.request.urlopen(url)
 print(response.code)
 print(response.headers)
@@ -23,4 +59,7 @@ print(response.info().get('Server'))
 print(url)
 response_content = response.read()
 #print(response_content)
+
+"""
+
 
