3f15b0ffbe375642f0457f0f245badfec9439f0d
Bethany Vella <bethany.vella10@gmail.com>
2022-03-11 14:34:32 +0100

    Useragent code

--- project.py
+++ project.py
@@ -4,6 +4,7 @@ import requests
 import urllib.request
 from csv import reader
 import pandas as pd
+import logging
 
 """
 #try 1 - BV - not really sure about this one..need to do more research
@@ -33,16 +34,31 @@ for count, url in df.iterrows():
 """
 read_obj = open('tranco_QYW4.csv', 'r') 
 csv_reader =  reader(read_obj)
-#csv_reader = ['google.com', 'youtube.com', 'facebook.com']
+logging.basicConfig(filename="logfile2.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='a')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+ 
 for count, url in enumerate(csv_reader):
     new_url = url[0] 
     try:
-        response = urllib.request.urlopen('https://'+new_url)
+        
+        req = urllib.request.Request(
+            'http://'+new_url,
+            data=None,
+            headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
+        )
+        response = urllib.request.urlopen(req)
+        
         print(new_url , response.info().get('Server'))
     except urllib.error.HTTPError as e:
         print(new_url , e.headers.get('Server'))
     except urllib.error.URLError as e:
         print(new_url, 'FAILED:', e.reason)
+        logger.info("Url Error - Website URL: %s Reason: %s",new_url, e.reason)
+    except ConnectionResetError as e:
+        continue
     
     if count == 99:
         break
